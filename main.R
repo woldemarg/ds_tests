9a9ec9bf6e7ab4b99a4263ffb2e09b931fcee3ed
woldemarg <wldmrgml@gmail.com>
2020-03-03 22:46:56 +0000

    plus weather

--- main.R
+++ main.R
@@ -7,6 +7,8 @@ events <- read_csv("data/encoded/events_Hokkaido.csv")
 holidays <- read_csv("data/encoded/holidays_Japan.csv")
 jalan <- read_csv("data/encoded/jalan_shinchitose.csv")
 rakuten <- read_csv("data/encoded/rakuten_shinchitose.csv")
+weather <- read_csv("data/encoded/weather_Hokkaido.csv")
 
 names(rakuten) <- description$`Row(EN)`[1:48]
 names(jalan) <- description$`Row(EN)`[49:94]
+
