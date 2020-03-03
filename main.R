69435e42396e4a7f281a7fc3647945924d8b16b6
woldemarg <wldmrgml@gmail.com>
2020-03-03 21:45:44 +0000

    script

---
+++ main.R
@@ -0,0 +1,12 @@
+#install.packages("tidyverse")
+
+library(tidyverse)
+
+description <- read_csv("data/encoded/columns_description.csv")
+events <- read_csv("data/encoded/events_Hokkaido.csv")
+holidays <- read_csv("data/encoded/holidays_Japan.csv")
+jalan <- read_csv("data/encoded/jalan_shinchitose.csv")
+rakuten <- read_csv("data/encoded/rakuten_shinchitose.csv")
+
+names(rakuten) <- description$`Row(EN)`[1:48]
+names(jalan) <- description$`Row(EN)`[49:94]
