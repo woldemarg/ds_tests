38fd081a55b24935a2e85d4fbb1f469c00a6413e
woldemarg <wldmrgml@gmail.com>
2020-03-04 21:26:56 +0000

    events

--- sripts/main.R
+++ sripts/main.R
@@ -25,7 +25,10 @@ holidays <- holidays %>%
   mutate(day = get_date(day, "Y/m/d"))
 
 events <- events %>%
-  mutate(start_date = get_date(start_date, "Y/m/d"))
+  mutate(
+    start_date = get_date(start_date, "Y/m/d"),
+    end_date = get_date(end_date, "Y/m/d"),
+  )
 
 jalan_filtered <- jalan %>%
   mutate(
@@ -89,15 +92,61 @@ rakuten_filtered <- rakuten %>%
 df_joined <- bind_rows(jalan_filtered, rakuten_filtered)
 df_joined$arrival_flight[df_joined$arrival_flight == "0"] <- NA
 
+sum_up_events <- function(date,
+                          city_id_excl = 1536,
+                          city_centre = c(141.650876, 42.8209577),
+                          data = events) {
+  events_in_neighbourhood <-
+    data %>% filter(city_id != city_id_excl & start_date == date)
+  
+  if (nrow(events_in_neighbourhood) == 0) {
+    ls <- list(
+      start_date = date,
+      cnt = 0,
+      dis = 0,
+      dur = 0
+    )
+    return(ls)
+  }
+  
+  events_in_neighbourhood$duration <- ifelse(
+    events_in_neighbourhood$end_date - events_in_neighbourhood$start_date == 0,
+    1,
+    events_in_neighbourhood$end_date - events_in_neighbourhood$start_date
+  )
+  
+  events_in_neighbourhood$distance <-
+    distm(events_in_neighbourhood[, 6:7], city_centre, fun = distHaversine)
+  
+  events_groupped <- events_in_neighbourhood %>%
+    group_by(start_date) %>%
+    summarise(cnt = n(),
+              dis = mean(distance),
+              dur = mean(duration)) %>%
+    ungroup()
+  
+  ls <- as.list(events_groupped[1,])
+}
+
+events_sum_up_ls <- lapply(df_joined$pickup_date, sum_up_events)
+
+events_sum_up_df <-
+  as_tibble(t(matrix(
+    unlist(events_sum_up_ls), nrow = length(unlist(f[1]))
+  )), .name_repair = "unique")
+
+names(events_sum_up_df) <- c("date",
+                             "events_count",
+                             "events_avg_distance",
+                             "events_avg_duration")
+
+events_sum_up_df$date <- as_date(events_sum_up_df$date)
+
 #according to https://en.wikipedia.org/wiki/Chitose,_Hokkaido
 #in japanese Chitose is 千歳市, correpondent city_id is 1536
 df_extended <- df_joined %>%
-  mutate(
-    is_weekend = ifelse(wday(pickup_date) %in% c(1, 7), 1, 0),
-    is_holiday = holidays$Japan[match(pickup_date, holidays$day)],
-    is_event = ifelse(pickup_date %in% events$start_date[events$city_id != 1536], 1, 0)
-  )
+  mutate(day_of_week = wday(pickup_date, label = TRUE),
+         is_holiday = holidays$Japan[match(pickup_date, holidays$day)]) %>%
+  left_join(events_sum_up_df, by = c("pickup_date" = "date"))
 
-
-city_centre <- c(141.650876, 42.8209577)
-d <- distm(events[1, 6:7], city_centre, fun = distHaversine)
+df_extended$month <- month(df_extended$pickup_date) 
