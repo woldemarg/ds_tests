d0f4e69b8d83e7dbabba7d95a237fcd2b0931246
woldemarg <wldmrgml@gmail.com>
2020-03-05 23:51:03 +0000

    mean vs rf

--- sripts/main.R
+++ sripts/main.R
@@ -96,6 +96,10 @@ rakuten_filtered <- rakuten %>%
 df_joined <- bind_rows(jalan_filtered, rakuten_filtered)
 df_joined$arrival_flight[df_joined$arrival_flight == "0"] <- NA
 
+dif <-
+  as.numeric(mean(df_joined$return_date - df_joined$pickup_date))
+
+dif
 #exploring weather conditions
 df_cancelled <- df_joined %>% filter(
   is_cancelled == 1 &
@@ -131,23 +135,22 @@ sum_up_events <- function(date,
                           city_centre = c(141.650876, 42.8209577),
                           data = events) {
   events_in_neighbourhood <-
-    data %>% filter(city_id != city_id_excl & start_date == date)
+    data %>% filter(city_id != city_id_excl & start_date == date &
+                      end_date - start_date < dif)
   
   if (nrow(events_in_neighbourhood) == 0) {
-    ls <- list(
-      start_date = date,
-      cnt = 0,
-      dis = 0,
-      dur = 0
-    )
-    return(ls)
+    ls <- list(start_date = date,
+               cnt = 0,
+               dis = 0
+               #dur = 0)
+               return(ls)
   }
   
-  events_in_neighbourhood$duration <- ifelse(
-    events_in_neighbourhood$end_date - events_in_neighbourhood$start_date == 0,
-    1,
-    events_in_neighbourhood$end_date - events_in_neighbourhood$start_date
-  )
+  # events_in_neighbourhood$duration <- ifelse(
+  #   events_in_neighbourhood$end_date - events_in_neighbourhood$start_date == 0,
+  #   1,
+  #   events_in_neighbourhood$end_date - events_in_neighbourhood$start_date
+  # )
   
   events_in_neighbourhood$distance <-
     distm(events_in_neighbourhood[, 6:7], city_centre, fun = distHaversine)
@@ -155,11 +158,11 @@ sum_up_events <- function(date,
   events_groupped <- events_in_neighbourhood %>%
     group_by(start_date) %>%
     summarise(cnt = n(),
-              dis = mean(distance),
-              dur = mean(duration)) %>%
-    ungroup()
-  
-  ls <- as.list(events_groupped[1,])
+              dis = mean(distance)
+              #dur = mean(duration)) %>%
+              ungroup()
+              
+              ls <- as.list(events_groupped[1, ])
 }
 
 events_sum_up_ls <-
@@ -172,159 +175,237 @@ events_sum_up_df <-
 
 names(events_sum_up_df) <- c("date",
                              "events_count",
-                             "events_avg_distance",
-                             "events_avg_duration")
-
-events_sum_up_df$date <- as_date(events_sum_up_df$date)
-
-
-#target encoding company by average price per day
-#
-# lookup <- df_joined %>% filter(is_cancelled == 0) %>%
-#   mutate(duration = ifelse(return_date - pickup_date == 0,
-#                            1,
-#                            return_date - pickup_date)) %>%
-#   group_by(company_name) %>%
-#   summarise(encoded_company = mean(total_price) / mean(duration)) %>%
-#   ungroup()
-
-
-#preparing data
-model_data <- df_joined %>%
-  filter(is_cancelled == 0) %>%
-  group_by(pickup_date, company_name) %>%
-  summarise(target = n())
-
-model_data_wide <- model_data %>%
-  pivot_wider(names_from = pickup_date, values_from = target)
-
-model_data_wide[is.na(model_data_wide)] <- 0
-
-model_data <- model_data_wide %>%
-  pivot_longer(names(model_data_wide)[-1],
-               names_to = "pickup_date",
-               values_to = "target")
-
-model_data$pickup_date <- as_date(model_data$pickup_date)
-
-model_data <- model_data %>%
-  mutate(
-    day_of_week = as_factor(as.character(wday(pickup_date, label = TRUE))),
-    month = as_factor(month(pickup_date)),
-    is_holiday = holidays$Japan[match(pickup_date, holidays$day)],
-    is_bad_weather = ifelse(pickup_date %in% bad_weather$date, 1, 0)
-  ) %>%
-  left_join(events_sum_up_df, by = c("pickup_date" = "date")) %>%
-  #left_join(lookup, by = "company_name") %>%
-  select(-pickup_date)
-
-model_data$company_name <- as_factor(model_data$company_name)
-
-rf_mod <- tuneRF(
-  x = model_data[names(model_data) != "target"],
-  y = model_data[["target"]],
-  ntreeTry = 1500,
-  stepFactor = 1.5,
-  improve = 1e-5,
-  #classwt = wt,
-  trace = FALSE,
-  plot = FALSE,
-  doBest = TRUE
-)
-
-str(model_data)
-
-train <-
-  dummy_cols(
-    model_data,
-    select_columns = c("company_name", "day_of_week", "month"),
-    remove_first_dummy = TRUE,
-    remove_selected_columns = TRUE
-  )
-
-str(train)
-
-cv_ridge <- cv.glmnet(
-  x = as.matrix(train[names(train) != "target"]),
-  y = train[["target"]],
-  nfolds = 10,
-  standardize = TRUE,
-  alpha = 0
-)
-
-cv_ridge
-
-cv_ridge$glmnet.fit$dev.ratio[which(cv_ridge$glmnet.fit$lambda == cv_ridge$lambda.min)]
-
-rf_mod
-
-varImpPlot(rf_mod, type = 2)
-
-change_pseudo_num_cols_to_factors <- function(data, lim = 15) {
-  sum <- get_summary(data)
-  pseudo_num <- sum %>%
-    filter(cls == "numeric" &
-             unq < lim &
-             !(fnm %in% c(ords$f, "OverallQual", "OverallCond"))) %>%
-    select(fnm) %>%
-    pull(fnm)
-  v <- model_data$events_avg_distance
-  n_vars <-
-    as_tibble(lapply(data[pseudo_num], function(v) {
-      t <- table(v)
-      p <- peaks(t)
-      p
-      n <- names(t)
-      n[p]
-      b <- as.numeric(c("0", n[p], Inf))
-      b
-      
-      b <- if (length(n[p]) == 0) {
-        c("-1", "0", n[length(n)])
-      } else {
-        if ("0" %in% n[p]) {
-          c("-1", n[p], n[length(n)])
-        } else {
-          if ("0" %in% n) {
-            c("-1", "0", n[p], n[length(n)])
-          } else {
-            c(n[1], n[p], n[length(n)])
-          }
-        }
-      }
-      return(cut(v, breaks = as.numeric(b), include.lowest = TRUE))
-    }))
-  
-  
-  #Randomly shuffle the data
-  d <- model_data[sample(nrow(model_data)), 1:4]
-  
-  #Create 10 equally size folds
-  folds <- cut(seq(1, nrow(d)), breaks = 10, labels = FALSE)
-  #Perform 10 fold cross validation
-  rsq <- function (x, y)
-    cor(x, y) ^ 2
-  
-  rmse <- c()
-  r2 <- c()
-  
-  for (i in 1:10) {
-    #Segement your data by fold using the which() function
-    testIndexes <- which(folds == i, arr.ind = TRUE)
-    testData <- d[testIndexes,]
-    trainData <- d[-testIndexes,]
-    #Use the test and train data partitions however you desire...
-    mean_model <- trainData %>%
-      group_by(company_name, month, day_of_week) %>%
-      summarize(predicts = mean(target))
-    
-    testData <- testData %>%
-      left_join(mean_model, by = c("company_name", "month", "day_of_week"))
-    
-    rmse[i] <- sqrt(sum(testData$predicts - testData$target) ^ 2)
-    
-    r2[i] <- rsq(testData$predicts, testData$target)
-  }
-  mean(rmse)
-  mean(r2)
-  
+                             "events_avg_distance"
+                             #"events_avg_duration")
+                             
+                             events_sum_up_df$date <- as_date(events_sum_up_df$date)
+                             
+                             
+                             #target encoding company by average price per day
+                             
+                              lookup <- df_joined %>% filter(is_cancelled == 0) %>%
+                                mutate(duration = ifelse(return_date - pickup_date == 0,
+                                                         1,
+                                                         return_date - pickup_date),
+                                       #is_weekend = ifelse(wday(pickup_date) %in% c(1,7), 1, 0),
+                                       day_of_week = as_factor(as.character(wday(pickup_date, label = TRUE)))) %>%
+                                group_by(company_name, day_of_week) %>%
+                                summarise(encoded_company = mean(total_price) / mean(duration)) %>%
+                                ungroup()
+                             
+                             
+                             #preparing data
+                             model_data <- df_joined %>%
+                               filter(is_cancelled == 0) %>%
+                               group_by(pickup_date, company_name) %>%
+                               summarise(target = n()) %>%
+                               ungroup()
+                             
+                             model_data_wide <- model_data %>%
+                               pivot_wider(names_from = pickup_date, values_from = target)
+                             
+                             model_data_wide[is.na(model_data_wide)] <- 0
+                             
+                             model_data <- model_data_wide %>%
+                               pivot_longer(names(model_data_wide)[-1],
+                                            names_to = "pickup_date",
+                                            values_to = "target")
+                             
+                             model_data$pickup_date <- as_date(model_data$pickup_date)
+                             
+                             model_data <- model_data %>%
+                               mutate(
+                                 day_of_week = as_factor(as.character(wday(pickup_date, label = TRUE))),
+                                 #is_weekend = ifelse(wday(pickup_date) %in% c(1,7), 1, 0),
+                                 month = as_factor(month(pickup_date)),
+                                 is_holiday = holidays$Japan[match(pickup_date, holidays$day)],
+                                 is_bad_weather = ifelse(pickup_date %in% bad_weather$date, 1, 0)
+                               ) %>%
+                               left_join(events_sum_up_df, by = c("pickup_date" = "date")) %>%
+                               #left_join(lookup, by = "company_name") %>%
+                               select(-pickup_date)
+                             
+                             model_data <- model_data %>%
+                               left_join(lookup, by = c("company_name", "day_of_week")) %>%
+                               select(-company_name)
+                             
+                             model_data$company_name <- as_factor(model_data$company_name)
+                             model_data$day_of_week <- as_factor(model_data$day_of_week)
+                             
+                             
+                             model_data2 <- model_data %>% select(-events_avg_distance)
+                             model_data$events_count <-
+                               as_factor(cut(v, breaks = as.numeric(b), include.lowest = TRUE))
+                             model_data$events_avg_distance <- as_factor(cut_number(v, 3))
+                             model_data$events_count <- as_factor(cut_number(v2, 3))
+                             
+                             model_data <- model_data %>% filter(company_name == "rakuten") %>%
+                               select(-company_name)
+                             
+                             model_data[sample(nrow(model_data)), 1:4]
+                             train <- d2[-testIndexes, ]
+                             test <- d2[testIndexes, ]
+                             
+                             train$events_count <-
+                               as_factor(cut(
+                                 train$events_count,
+                                 breaks = as.numeric(b),
+                                 include.lowest = TRUE
+                               ))
+                             test$events_count <-
+                               as_factor(cut(
+                                 test$events_count,
+                                 breaks = as.numeric(b),
+                                 include.lowest = TRUE
+                               ))
+                             
+                             rf_mod <- tuneRF(
+                               x = train[names(train) != "target"],
+                               y = train[["target"]],
+                               ntreeTry = 500,
+                               stepFactor = 1.5,
+                               improve = 1e-5,
+                               #classwt = wt,
+                               trace = FALSE,
+                               plot = FALSE,
+                               doBest = TRUE
+                             )
+                             
+                             preds <- predict(rf_mod, test[, -2], ntree = 500)
+                             sqrt(mean(preds - test$target) ^ 2)
+                             rmse
+                             test$preds <- preds
+                             testData$rf <- preds
+                             str(model_data)
+                             
+                             train2 <-
+                               dummy_cols(
+                                 train,
+                                 select_columns = c("company_name", "month", "day_of_week", "events_count"),
+                                 remove_first_dummy = TRUE,
+                                 remove_selected_columns = TRUE
+                               )
+                             
+                             str(train)
+                             
+                             cv_ridge <- cv.glmnet(
+                               x = as.matrix(train[names(train) != "target"]),
+                               y = train[["target"]],
+                               nfolds = 10,
+                               standardize = TRUE,
+                               alpha = 0
+                             )
+                             
+                             cv_ridge
+                             
+                             cv_ridge$glmnet.fit$dev.ratio[which(cv_ridge$glmnet.fit$lambda == cv_ridge$lambda.min)]
+                             
+                             rf_mod
+                             sqrt(tail(rf_mod$mse, 1))
+                             sqrt(rf_mod$mse[length(rf_mod$mse)])
+                             
+                             varImpPlot(rf_mod, type = 2)
+                             
+                             change_pseudo_num_cols_to_factors <- function(data, lim = 15) {
+                               sum <- get_summary(data)
+                               pseudo_num <- sum %>%
+                                 filter(cls == "numeric" &
+                                          unq < lim &
+                                          !(fnm %in% c(ords$f, "OverallQual", "OverallCond"))) %>%
+                                 select(fnm) %>%
+                                 pull(fnm)
+                               v <- model_data$events_avg_distance
+                               v2 <- model_data$events_count
+                               range(v)
+                               n_vars <-
+                                 as_tibble(lapply(data[pseudo_num], function(v) {
+                                   t <- table(v)
+                                   t
+                                   p <- peaks(t)
+                                   p
+                                   n <- names(t)
+                                   n[p]
+                                   b <- as.numeric(c("0", n[p], Inf))
+                                   b
+                                   
+                                   b <- if (length(n[p]) == 0) {
+                                     c("-1", "0", n[length(n)])
+                                   } else {
+                                     if ("0" %in% n[p]) {
+                                       c("-1", n[p], n[length(n)])
+                                     } else {
+                                       if ("0" %in% n) {
+                                         c("-1", "0", n[p], n[length(n)])
+                                       } else {
+                                         c(n[1], n[p], n[length(n)])
+                                       }
+                                     }
+                                   }
+                                   return(cut(v, breaks = as.numeric(b), include.lowest = TRUE))
+                                 }))
+                               
+                               
+                               #Randomly shuffle the data
+                               d2 <- model_data[sample(nrow(model_data)), ]
+                               d <- d2 %>% select(target, encoded_company, month, day_of_week)
+                               #Create 10 equally size folds
+                               folds <- cut(seq(1, nrow(d)), breaks = 10, labels = FALSE)
+                               #Perform 10 fold cross validation
+                               rsq <- function (x, y) {
+                                 cor(x, y) ^ 2}
+                               
+                               rmse <- c()
+                               r2 <- c()
+                               
+                               for (i in 1:10) {
+                                 #Segement your data by fold using the which() function
+                                 testIndexes <- which(folds == i, arr.ind = TRUE)
+                                 testData <- d[testIndexes, ]
+                                 trainData <- d[-testIndexes, ]
+                                 #Use the test and train data partitions however you desire...
+                                 mean_model <- trainData %>%
+                                   group_by(encoded_company, month, day_of_week) %>%
+                                   summarize(predicts = mean(target))
+                                 
+                                 testData <- testData %>%
+                                   left_join(mean_model, by = c("encoded_company", "month", "day_of_week"))
+                                 
+                                 rmse[i] <- sqrt(mean(testData$predicts - testData$target) ^ 2)
+                                 
+                                 r2[i] <- rsq(testData$predicts, testData$target)
+                               }
+                               mean(rmse)
+                               mean(r2)
+                               rmse
+                               
+                               
+                               rmse2 <- c()
+                               
+                               for (i in 1:10) {
+                                 #Segement your data by fold using the which() function
+                                 testIndexes2 <- which(folds == i, arr.ind = TRUE)
+                                 testData2 <- d2[testIndexes2, ]
+                                 trainData2 <- d2[-testIndexes2, ]
+                                 #Use the test and train data partitions however you desire...
+                                 rf_mod <- tuneRF(
+                                   x = trainData2[names(trainData2) != "target"],
+                                   y = trainData2[["target"]],
+                                   ntreeTry = 500,
+                                   stepFactor = 1.5,
+                                   improve = 1e-5,
+                                   #classwt = wt,
+                                   trace = FALSE,
+                                   plot = FALSE,
+                                   doBest = TRUE
+                                 )
+                                 
+                                 preds <- predict(rf_mod, testData2[, -1], ntree = 500)
+                                 
+                                 
+                                 rmse2[i] <- sqrt(mean(preds - testData2$target) ^ 2)
+                               }
+                               mean(rmse2)
+                               mean(rmse)
+                               rmse2
+                               rmse
+                               
