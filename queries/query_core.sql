b604f5fd36ea7f24643a97dd15edd6b9383b959e
Volodymyr Holomb <wldmrgml@gmail.com>
2020-04-30 18:11:14 +0300

    error

--- queries/query_core.sql
+++ queries/query_core.sql
@@ -12,12 +12,4 @@ SELECT `date`,
        `worker_id`, 
        'estimated' AS `type` 
 FROM   `public.dailyplan` 
-WHERE  `project_id` <> '') 
-WHERE `date` BETWEEN '2020-02-17' AND '2020-02-21' 
-AND `type` = 
-CASE 
-WHEN `date` < '2020-02-21' THEN 
-  'spent' 
-ELSE 
-  'estimated' 
-end;
+WHERE  `project_id` <> '';
