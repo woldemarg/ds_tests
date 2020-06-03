49e8303ce3f5c01b6574d956375765c39d40729e
Volodymyr Holomb <wldmrgml@gmail.com>
2020-06-03 13:16:47 +0300

    change func

--- data_analysis/company_4/task_solution/scripts/main.py
+++ data_analysis/company_4/task_solution/scripts/main.py
@@ -14,14 +14,14 @@ def get_confidence_ab(df):
     std_old = np.sqrt(rate_old * (1 - rate_old) / df.iloc[0, 1])
     std_new = np.sqrt(rate_new * (1 - rate_new) / df.iloc[1, 1])
     z_score = (rate_new - rate_old) / np.sqrt(std_old**2 + std_new**2)
-    return norm.cdf(z_score)
+    return rate_old, rate_new, norm.cdf(z_score)
 
 # %%
 def print_ab_results(data,
                      key,
                      strategy="periods",
                      freq="1M",
-                     param_list=None):
+                     param_list=[]):
     if strategy == "periods":
         groups = (data
                   .groupby(["experiment_mobile_checkout_theme",
@@ -37,16 +37,22 @@ def print_ab_results(data,
                             sort_remaining=True,
                             ascending=False))
 
-    if len(param_list) > 1:
+    if len(param_list) == 0 or len(param_list) > 1:
         for idx, df_select in groupped.groupby(level=-1, axis=0):
-            print("B's conversion as for '{}' estimated by '{}' is better with {:.1%} confidence"
-                  .format(idx, key, get_confidence_ab(df_select)))
+            rate_A, rate_B, conf = get_confidence_ab(df_select)
+            if rate_B <= rate_A:
+                print("Seems like B performs worser on {}\n."
+                      .format(key))
+            else:
+                print("A conversion rate as for '{i}' estimated by '{k}' is {a:.3}.\nB conversion rate as for '{i}' estimated by '{k}' is {b:.3}.\nDifference is significant with {c:.2%} confidence.\n\n"""
+                      .format(i=idx, k=key, a=rate_A, b=rate_B, c=conf))
     else:
-        print("B's conversion estimated by '{}' is better with {:.1%} confidence"
-              .format(key, get_confidence_ab(groupped)))
+        rate_A, rate_B, conf = get_confidence_ab(groupped)
+        print("A conversion rate estimated by '{k}' is {a:.3}.\nB conversion rate estimated by '{k}' is {b:.3}.\nDifference is significant with {c:.2%} confidence.\n\n"""
+                      .format(k=key, a=rate_A, b=rate_B, c=conf))
 
 # %%
 print_ab_results(data=exp_data,
                  key="transaction_try",
                  strategy="features",
-                 param_list=["experiment_mobile_checkout_theme"])
+                param_list=["experiment_mobile_checkout_theme"])
