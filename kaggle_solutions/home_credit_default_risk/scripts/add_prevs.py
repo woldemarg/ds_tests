78afab15b4ce30fe4be1e6ef9a7aa26e2d9e05a0
Volodymyr Holomb <wldmrgml@gmail.com>
2020-06-29 17:37:30 +0300

    rest

--- kaggle_solutions/home_credit_default_risk/scripts/add_prevs.py
+++ kaggle_solutions/home_credit_default_risk/scripts/add_prevs.py
@@ -22,8 +22,8 @@ credit_card_balance = pd.read_csv("kaggle_solutions/home_credit_default_risk/dat
                                   index_col=0)
 
 # %%
-previous_application = pd.read_csv("kaggle_solutions/home_credit_default_risk/data/samples/prev_app_samp.csv.gz",
-                                   index_col=0)
+p_apps = pd.read_csv("kaggle_solutions/home_credit_default_risk/data/samples/prev_app_samp.csv.gz",
+                     index_col=0)
 
 # %%
 installments_payments = pd.read_csv("kaggle_solutions/home_credit_default_risk/data/samples/in_pay_samp.csv.gz",
@@ -44,10 +44,10 @@ def get_stats(df):
     return stats
 
 # %%
-previous_application_stats = get_stats(previous_application)
+p_apps_stats = get_stats(p_apps)
 
 # %%
-contract_types = (previous_application["NAME_CONTRACT_TYPE"]
+contract_types = (p_apps["NAME_CONTRACT_TYPE"]
                   .unique()
                   .tolist())
 
@@ -61,8 +61,8 @@ def make_intertable(rows, cols):
     for t in list(itertools
                   .product(rows, cols)):
         b = (globals()[t[1]]["SK_ID_PREV"]
-             .isin(previous_application
-                   .loc[previous_application["NAME_CONTRACT_TYPE"] == t[0],
+             .isin(p_apps
+                   .loc[p_apps["NAME_CONTRACT_TYPE"] == t[0],
                         "SK_ID_PREV"]).any())
         ls.append(b)
     out = pd.DataFrame(data=(np.array(ls)
@@ -75,5 +75,29 @@ def make_intertable(rows, cols):
 
 intertable = make_intertable(rows=contract_types,
                              cols=tables_names)
+
+# %%
+p_apps_mod = (p_apps
+              .query("FLAG_LAST_APPL_PER_CONTRACT == 'Y'\
+                     and NFLAG_LAST_APPL_IN_DAY == '1'")
+              .copy())
+
+# %%
+psb_term = (pos_cash_balance
+            .groupby("SK_ID_PREV")
+            .apply(lambda d:
+                   d.loc[d["NAME_CONTRACT_STATUS"] == "Completed",
+                         "MONTHS_BALANCE"].max() * 30)
+            .rename("DAYS_TERM_UPD"))
+
+p_apps_mod = pd.merge(left=previous_application_mod,
+                      right=psb_term,
+                      how="left",
+                      left_on="SK_ID_PREV",
+                      right_index=True)
+
+p_apps_mod["DAYS_TERM_UPD"] = [max(t) for t in
+                               list(zip(p_apps_mod["DAYS_TERMINATION"],
+                                        p_apps_mod["DAYS_TERM_UPD"]))]
 
 # %%
